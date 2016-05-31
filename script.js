ee75c98a34dfbfa0047fc5f5752856e61d22cc92
Serhii <s.krokhmalnyi@gmail.com>
2016-05-31 17:52:05 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+var width = window.innerWidth;
+var fibonacci = [0, 1];
+var output = document.getElementById('output');
+output.width = width;
+function isNumber(e){
+    e = e || window.event;
+    var charCode = e.which ? e.which : e.keyCode;
+    return /\d/.test(String.fromCharCode(charCode));
+}
+function fib() {
+	var n = document.getElementById('positionFib').value;
+	for (var i = 2; i < n; i ++) {
+  		fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];
+	}
+	document.getElementById('output').innerHTML = fibonacci.slice(0,n);
+}
